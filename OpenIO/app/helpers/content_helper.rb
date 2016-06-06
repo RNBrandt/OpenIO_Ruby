e8e73a237071f387235c0c7b47d5a554625ee681
Reuben Brandt <Reubennbrandt@gmail.com>
2016-06-06 13:26:04 -0700

    Add list all objects, get object info and create object methods to client object

--- OpenIO/app/helpers/content_helper.rb
+++ OpenIO/app/helpers/content_helper.rb
@@ -1,7 +1,7 @@
 module ContentHelper
 
   class Client
-    attr_reader :namespace, :server_host, :containers
+    attr_reader :namespace, :server_host, :containers, :list_all_objects
     def initialize(namespace, server_host)
       @namespace = namespace
       @server_host = server_host
@@ -17,10 +17,40 @@ module ContentHelper
 
     def locate_container(container_url)
       @containers.each do |container|
-
+        if container.url == container_url
+          return container
+        else
+          p "That container is not in this client"
+        end
       end
     end
 
+    def create_object(container_url, object_name, object_type, object_url)
+      target = self.locate_container(container_url)
+      target.new_object(object_name, object_type, object_url)
+    end
+
+    def list_all_objects
+      @list_all_objects = []
+      #This is not an efficient way to do this On^2, it can be done better through a sql database, but barring that, this will work.
+      @containers.each do |container|
+        container.objects.each do |object|
+          @list_all_objects << object
+        end
+        return @list_all_objects
+      end
+    end
+
+    def get_object_info(object_url)
+       @containers.each do |container|
+        container.objects.each do |object|
+          if object.url == object_url
+            return object
+          else
+            p "That object is not in this client"
+          end
+      end
+    end
   end
 
   class Container
@@ -33,6 +63,9 @@ module ContentHelper
     end
 
     def new_object(name, type, url)
+      temp = Object.new(name, type, url)
+      @objects << temp
+    end
   end
     class Object
       def initialize(name, type, url)
